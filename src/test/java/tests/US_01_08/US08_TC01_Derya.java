5cec70f0cd28d2c3f0a28adde302fdfab0e17ece
DeryaDursun <derdursun55@gmail.com>
2023-06-05 14:51:48 +0300

    Derya_v3

---
+++ src/test/java/tests/US_01_08/US08_TC01_Derya.java
@@ -0,0 +1,11 @@
+package tests.US_01_08;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class US08_TC01_Derya {
+
+}
