cb7b5ffef0e9da2bcb6cc92691e4655c1050b470
DeryaDursun <derdursun55@gmail.com>
2023-06-05 15:50:48 +0300

    Derya_US_TC01-V1

--- src/test/java/pages/tripandwayPage.java
+++ src/test/java/pages/tripandwayPage.java
@@ -9,12 +9,28 @@ public class tripandwayPage {
     public tripandwayPage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+
     //Home Page kısmında sol üstte bulunan Logo locaterı
     @FindBy(xpath = "//*[@alt='Logo']")
     public WebElement tripandwayLogo;
 
     @FindBy(xpath = "//h3[text()='Recent Posts']")
     public WebElement recentPostsElementi;
+
+
+    @FindBy(xpath = "//*[text()='Address']")
+    public WebElement addressElementi;
+
+
+    @FindBy(xpath = "//*[text()='34, Edd Lane, NYC, 22335']")
+    public WebElement adressBilgisi;
+
+    @FindBy(xpath = "(//p[text()='202-456-3789'])[2]")
+    public WebElement telefonBilgisi;
+
+    @FindBy(xpath = "(//p[text()='info@tripandway.com'])[2]")
+    public WebElement emailBilgisi;
 
 
 
--- src/test/java/tests/US_01_08/US08_TC01_Derya.java
+++ src/test/java/tests/US_01_08/US08_TC01_Derya.java
@@ -3,9 +3,39 @@ package tests.US_01_08;
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
+import pages.tripandwayPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class US08_TC01_Derya {
+        @Test
+        public void Test01() throws InterruptedException {
 
-}
+            //Sayfaya gidilir
+            Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
+
+            //Sayfanın Footer bölümüne inilir
+            tripandwayPage tripandwayPages = new tripandwayPage();
+
+            JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+            jse.executeScript("arguments[0].scrollIntoView();", tripandwayPages.addressElementi);
+            Thread.sleep(3000);
+
+            //Adres bilgisinin görünür oldugu dogrulanır
+            SoftAssert softAssert = new SoftAssert();
+            softAssert.assertTrue(tripandwayPages.adressBilgisi.isDisplayed());
+            System.out.println(tripandwayPages.adressBilgisi.getText());
+            Thread.sleep(3000);
+
+            //Telefon bilgisinin görünür oldugu dogrulanır
+            softAssert.assertTrue(tripandwayPages.telefonBilgisi.isDisplayed());
+            System.out.println(tripandwayPages.telefonBilgisi.getText());
+            Thread.sleep(2000);
+
+            //e-mail bilgisinin görünür oldugu dogrulanır
+            softAssert.assertTrue(tripandwayPages.emailBilgisi.isDisplayed());
+            System.out.println(tripandwayPages.emailBilgisi.getText());
+            softAssert.assertAll();
+
+        }
+    }
