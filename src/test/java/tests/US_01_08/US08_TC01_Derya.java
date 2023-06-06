f2c3bd96dc1ec0f27512966779da07d9a0a0a207
DeryaDursun <derdursun55@gmail.com>
2023-06-06 21:17:03 +0300

    Derya.06.06.2023

--- src/test/java/tests/US_01_08/US08_TC01_Derya.java
+++ src/test/java/tests/US_01_08/US08_TC01_Derya.java
@@ -11,25 +11,25 @@ public class US08_TC01_Derya {
     @Test
     public void Test01() throws InterruptedException {
 
-        //ADRES BİLGİLERİNİN GÖRÜNÜRLÜĞÜNÜ DOGRULAMA
+        //ADRES BİLGİLERİNİN GÖRÜNÜRLÜĞÜNÜ DOGRULAMA....
 
-        //Sayfaya gidilir
+        //Sayfaya gidilir...
         Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
 
-        //Sayfanın Footer bölümüne inilir
+        //Sayfanın Footer bölümüne inilir..
         tripandwayPage tripandwayPage = new tripandwayPage();
 
         JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
         jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.addressElementi);
         Thread.sleep(3000);
 
-        //Adres bilgisinin görünür oldugu dogrulanır
+        //Adres bilgisinin görünür oldugu dogrulanır....g
         SoftAssert softAssert=new SoftAssert();
         softAssert.assertTrue(tripandwayPage.adressBilgisi.isDisplayed());
         System.out.println(tripandwayPage.adressBilgisi.getText());
         Thread.sleep(3000);
 
-        //Telefon bilgisinin görünür oldugu dogrulanır
+        //Telefon bilgisinin görünür oldugu dogrulanır...
         softAssert.assertTrue(tripandwayPage.telefonBilgisi.isDisplayed());
         System.out.println(tripandwayPage.telefonBilgisi.getText());
         Thread.sleep(2000);
