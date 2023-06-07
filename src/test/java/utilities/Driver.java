ec41c84a18ad64ed313562565fca530f538b70e2
DeryaDursun <derdursun55@gmail.com>
2023-06-07 22:46:08 +0300

    son

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -4,27 +4,64 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 import java.time.Duration;
 
 public class Driver {
-    static WebDriver driver;
+    private static WebDriver driver;
+    private Driver(){
+        /*Singleton pattern kullanilarak
+        istenmeyen yöntemlerle driver objesine
+        erisilmesini engelledik
 
-    public static WebDriver getDriver(){
+        Constructer'i private yaparak bu classdan
+        obje olusturularak class uyelerinin kullanilmasinin
+        onune gectik.
+
+         */
+
+    }
+
+    public static WebDriver getDriver() {
+
+        String istenenBrowser = ConfigReader.getProperty("browser");
 
 
-        if(driver == null){
+        if (driver == null) {
 
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+
+
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-        }
+
 
         return driver;
     }
 
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.close();
+            driver = null;
 
-    public static void closeDriver(){
-        driver.close();
+        }
     }
+    public static void quitDriver(){
+        if(driver!=null){
+            driver.quit();
+            driver=null;
+
+
+        }
+
+
+
+
+    }
+
+
 }
+
