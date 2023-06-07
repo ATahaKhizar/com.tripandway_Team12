f99254dc49f8452c6a0ebb0eda6ffa2b1eb0a621
DeryaDursun <derdursun55@gmail.com>
2023-06-07 13:52:43 +0300

    07.06.2023_Derya

--- src/test/java/tests/US_01_08/US07_TC01_Derya.java
+++ src/test/java/tests/US_01_08/US07_TC01_Derya.java
@@ -1,6 +1,7 @@
 package tests.US_01_08;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pages.TripandwayPage;
@@ -14,9 +15,8 @@ public class US07_TC01_Derya {
     public void Test01() throws InterruptedException {
 
         //SOSYAL MEDYA IKONLARİNİN GORUNDUGUNU DOGRULAMA
-
         //Sayfaya gidilir
-        Driver.getDriver().get(ConfigReader.getProperty("tripandwayUrl"));
+        Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
 
         //Sayfanın Footer bölümüne inilir
         TripandwayPage tripandwayPage = new TripandwayPage();
@@ -53,8 +53,8 @@ public class US07_TC01_Derya {
         //Instagram butonunun görünürlüğü dogrulanır
 
         softAssert.assertTrue(tripandwayPage.instagramElementi.isDisplayed());
-
-        Thread.sleep(3000);
+        softAssert.assertAll();
+        Driver.closeDriver();
 
     }
 
@@ -63,8 +63,9 @@ public class US07_TC01_Derya {
 
         //SOSYAL MEDYA IKONlARININ AKTIFLIGINI DOGRULAMA
 
+        //Facebook butonunun aktifligini dogrulama
         //Sayfaya gidilir
-        Driver.getDriver().get(ConfigReader.getProperty("tripandwayUrl"));
+        Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
 
         //Sayfanın Footer bölümüne inilir
         TripandwayPage tripandwayPage = new TripandwayPage();
@@ -79,135 +80,179 @@ public class US07_TC01_Derya {
 
         //Facebook sayfasının görünürlüğü dogrulanır
 
-        String mainHandle=Driver.getDriver().getWindowHandle();
-        Set<String> allWindowHandles1=Driver.getDriver().getWindowHandles();
+        String mainHandle = Driver.getDriver().getWindowHandle();
+        Set<String> allWindowHandles1 = Driver.getDriver().getWindowHandles();
 
-        for (String each:allWindowHandles1) {
+        for (String each : allWindowHandles1) {
             Driver.getDriver().switchTo().window(each);
             //System.out.println("Current title while switching windows :"+ Driver.getDriver().getTitle());
 
         }
 
-        String expectedTitle1="Facebook";
-        String actualTitle1=Driver.getDriver().getTitle();
+        String expectedTitle1 = "Facebook";
+        String actualTitle1 = Driver.getDriver().getTitle();
 
         System.out.println(expectedTitle1);
         System.out.println(actualTitle1);
 
-        SoftAssert softAssert=new SoftAssert();
-        softAssert.assertTrue(actualTitle1.contains(expectedTitle1));
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(actualTitle1.contains(expectedTitle1), "Facebook adresine gitmedi");
+        softAssert.assertAll();
 
-        //Twitter butonu seçilerek ilgili sayfaya gidilir
+        Driver.closeDriver();
+    }
+        @Test
+        public void Test03() throws InterruptedException {
 
-        Driver.getDriver().switchTo().window(mainHandle);
-        Thread.sleep(3000);
-        jse = (JavascriptExecutor) Driver.getDriver();
-        jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.twitterElementi);
-        Thread.sleep(1000);
-        tripandwayPage.twitterElementi.click();
-        Thread.sleep(3000);
+            //Twitter butonunun aktifliğini dogrulama
 
-        //Twitter sayfasının görünürlüğü dogrulanır
+            //Sayfaya gidilir
+            Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
 
-        mainHandle=Driver.getDriver().getWindowHandle();
-        Set<String> allWindowHandles2=Driver.getDriver().getWindowHandles();
+            //Sayfanın Footer bölümüne inilir
+            TripandwayPage tripandwayPage = new TripandwayPage();
 
-        for (String each:allWindowHandles2) {
-            Driver.getDriver().switchTo().window(each);
+            //Twitter butonu seçilerek ilgili sayfaya gidilir
 
+            JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+            jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.twitterElementi);
+            Thread.sleep(1000);
+            tripandwayPage.twitterElementi.click();
+            Thread.sleep(3000);
+
+            //Twitter sayfasının görünürlüğü dogrulanır
+
+            String mainHandle = Driver.getDriver().getWindowHandle();
+            Set<String> allWindowHandles2 = Driver.getDriver().getWindowHandles();
+
+            for (String each : allWindowHandles2) {
+                Driver.getDriver().switchTo().window(each);
+
+            }
+
+            String expectedTitle2 = "Keşfet/Twitter";
+            String actualTitle2 = Driver.getDriver().getTitle();
+
+            System.out.println(expectedTitle2);
+            System.out.println(actualTitle2);
+            SoftAssert softAssert=new SoftAssert();
+            softAssert.assertTrue(actualTitle2.contains(expectedTitle2), "Twitter sayfasina gitmedi");
+            softAssert.assertAll();
+            Driver.closeDriver();
         }
 
-        String expectedTitle2="Keşfet/Twitter";
-        String actualTitle2=Driver.getDriver().getTitle();
+        @Test
+        public void Test04() throws InterruptedException {
+            //Linkedin butonunun aktifliğini dogrulama
 
-        System.out.println(expectedTitle2);
-        System.out.println(actualTitle2);
+            //Sayfaya gidilir
+            Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
 
-        softAssert.assertTrue(actualTitle2.contains(expectedTitle2));
-        //softAssert.assertFalse(!(actualTitle2.contains(expectedTitle2)));
+            //Sayfanın Footer bölümüne inilir
 
-        //Linkedln butonu seçilerek ilgili sayfaya gidilir
-        Driver.getDriver().switchTo().window(mainHandle);
-        Thread.sleep(3000);
-        jse = (JavascriptExecutor) Driver.getDriver();
-        jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.linkedinElementi);
-        Thread.sleep(1000);
-        tripandwayPage.linkedinElementi.click();
-        Thread.sleep(3000);
+            TripandwayPage tripandwayPage = new TripandwayPage();
+            JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+            jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.linkedinElementi);
+            Thread.sleep(3000);
 
-        //Linkedln sayfasının görünürlüğü dogrulanır
-        mainHandle=Driver.getDriver().getWindowHandle();
-        Set<String> allWindowHandles3=Driver.getDriver().getWindowHandles();
+            //Linkedln butonu seçilerek ilgili sayfaya gidilir
+            tripandwayPage.linkedinElementi.click();
+            Thread.sleep(3000);
 
-        for (String each:allWindowHandles3) {
-            Driver.getDriver().switchTo().window(each);
+            //Linkedln sayfasının görünürlüğü dogrulanır
+            String mainHandle = Driver.getDriver().getWindowHandle();
+            Set<String> allWindowHandles3 = Driver.getDriver().getWindowHandles();
+
+            for (String each : allWindowHandles3) {
+                Driver.getDriver().switchTo().window(each);
+            }
+
+            String expectedTitle3 = "LinkedIn";
+            String actualTitle3 = Driver.getDriver().getTitle();
+
+            System.out.println(expectedTitle3);
+            System.out.println(actualTitle3);
+            Assert.assertTrue(actualTitle3.contains(expectedTitle3));
+            Driver.closeDriver();
         }
 
-        String expectedTitle3="LinkedIn";
-        String actualTitle3=Driver.getDriver().getTitle();
+        @Test
+        public void Test05() throws InterruptedException {
 
-        System.out.println(expectedTitle3);
-        System.out.println(actualTitle3);
+            //Pinterest butonunun aktifliğini dogrulama
 
-        softAssert.assertTrue(actualTitle3.contains(expectedTitle3));
+            //Sayfaya gidilir
+            Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
 
+            //Sayfanın Footer bölümüne inilir
+            TripandwayPage tripandwayPage = new TripandwayPage();
+            JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+            jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.pinterestElementi);
+            Thread.sleep(1000);
+            tripandwayPage.pinterestElementi.click();
+            Thread.sleep(3000);
 
-        //Pinterest butonu seçilerek ilgili sayfaya gidilir
-        Driver.getDriver().switchTo().window(mainHandle);
-        Thread.sleep(3000);
-        jse = (JavascriptExecutor) Driver.getDriver();
-        jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.twitterElementi);
-        Thread.sleep(1000);
-        tripandwayPage.pinterestElementi.click();
-        Thread.sleep(3000);
+            //Pinterest sayfasının görünürlüğü dogrulanır
+            String mainHandle = Driver.getDriver().getWindowHandle();
+            Set<String> allWindowHandles4 = Driver.getDriver().getWindowHandles();
 
-        //Pinterest sayfasının görünürlüğü dogrulanır
-        mainHandle=Driver.getDriver().getWindowHandle();
-        Set<String> allWindowHandles4=Driver.getDriver().getWindowHandles();
+            for (String each : allWindowHandles4) {
+                Driver.getDriver().switchTo().window(each);
+            }
 
-        for (String each:allWindowHandles4) {
-            Driver.getDriver().switchTo().window(each);
+            String expectedTitle4 = "Pinterest";
+            String actualTitle4 = Driver.getDriver().getTitle();
+
+            System.out.println(expectedTitle4);
+            System.out.println(actualTitle4);
+            SoftAssert softAssert=new SoftAssert();
+            softAssert.assertTrue(actualTitle4.contains(expectedTitle4), "Pinterest sayfasina gitmedi");
+            softAssert.assertAll();
+            Driver.closeDriver();
         }
 
-        String expectedTitle4="Pinterest";
-        String actualTitle4=Driver.getDriver().getTitle();
+        @Test
+        public void Test06() throws InterruptedException {
 
-        System.out.println(expectedTitle4);
-        System.out.println(actualTitle4);
+        //Instagram butonunun aktifliğini dogrulama
 
-        softAssert.assertTrue(actualTitle4.contains(expectedTitle4));
-        //softAssert.assertFalse(!(actualTitle4.contains(expectedTitle4)));
+       //Sayfaya gidilir
+
+            Driver.getDriver().get(ConfigReader.getProperty("tripandwayURL"));
+
+            //Sayfanın Footer bölümüne inilir
+            TripandwayPage tripandwayPage = new TripandwayPage();
 
 
-        //Instagram butonu seçilerek ilgili sayfaya gidilir
-        Driver.getDriver().switchTo().window(mainHandle);
-        Thread.sleep(3000);
-        jse = (JavascriptExecutor) Driver.getDriver();
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
         jse.executeScript("arguments[0].scrollIntoView();", tripandwayPage.instagramElementi);
-        Thread.sleep(1000);
+        Thread.sleep(3000);
         tripandwayPage.instagramElementi.click();
         Thread.sleep(3000);
 
         //Instagram sayfasının görünürlüğü dogrulanır
-        mainHandle=Driver.getDriver().getWindowHandle();
-        Set<String> allWindowHandles5=Driver.getDriver().getWindowHandles();
+        String mainHandle = Driver.getDriver().getWindowHandle();
+        Set<String> allWindowHandles5 = Driver.getDriver().getWindowHandles();
 
-        for (String each:allWindowHandles5) {
+        for (String each : allWindowHandles5) {
             Driver.getDriver().switchTo().window(each);
         }
 
-        String expectedTitle5="Instagram";
-        String actualTitle5=Driver.getDriver().getTitle();
+        String expectedTitle5 = "Instagram";
+        String actualTitle5 = Driver.getDriver().getTitle();
 
         System.out.println(expectedTitle5);
         System.out.println(actualTitle5);
 
-        softAssert.assertTrue(actualTitle5.contains(expectedTitle5));
 
+        SoftAssert softAssert=new SoftAssert();
+        softAssert.assertTrue(actualTitle5.contains(expectedTitle5), "Instagram sayfasina gitmedi");
 
-
+        softAssert.assertAll();
+        Driver.closeDriver();
 
 
     }
+    }
 
-}
+
