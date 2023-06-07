c3fb79799a4e796434b287fb580e7108828b50b9
zsk <zsk199207@gmail.com>
2023-06-07 23:00:51 +0300

    driver_guncellendi

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -4,22 +4,34 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 import java.time.Duration;
 
 public class Driver {
-    static WebDriver driver;
+
+    private static WebDriver driver;
+    private Driver(){
+       /*
+       Singleton pattern kullanilarak istenmeyen yontemlerle
+       driver objesine erisilmesini engelledik.
+
+       Constructor'i private yaparak bu class' dan obje olusturularak
+       class uyelerinin kullanilmasinin onune gectik.
+       */
+    }
 
     public static WebDriver getDriver(){
-
 
         String istenenBrowser = ConfigReader.getProperty("browser");
         // chrome, firefox, safari, edge
 
 
-        if(driver == null){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-         }
+        if (driver==null){
+
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
@@ -27,20 +39,21 @@ public class Driver {
         return driver;
     }
 
-
     public static void closeDriver(){
 
         if (driver != null){
             driver.close();
-            driver=null;
+            driver = null;
         }
+
     }
 
     public static void quitDriver(){
 
         if (driver != null){
             driver.quit();
-            driver=null;
+            driver = null;
         }
+
     }
 }
